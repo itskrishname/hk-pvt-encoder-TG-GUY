a9a0356ee1f8f4135ac303a70ee41a0835cedc0c
Krishna <kd259457@gmail.com>
2025-08-25 13:00:00 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,34 @@
+from bot.get_cfg import get_config
+
+class Config(object):
+    # You can keep this default
+    SESSION_NAME = get_config("SESSION_NAME", "EncoderX") 
+    # EncoderX_bot....
+    # sucks Dude
+    APP_ID = int(get_config("APP_ID", "26387127"))
+    API_HASH = get_config("API_HASH", "19718ab7acd97d0f71ada2807ddfe47a")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "Sheikhhasinarthemodhirbow")
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+     # Get these values from my.telegram.org
+    AUTH_USERS = [8154074711]
+# array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🌹
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8439914112:AAFaXhDqtK2HksThM1HYQGwCF6dJRHME1T4")
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
+    # Telegram maximum file upload size
+    BOT_USERNAME = get_config("BOT_USERNAME", "Tjggursgjybot")
+    MAX_FILE_SIZE = 4194304000
+    TG_MAX_FILE_SIZE = 4194304000
+    FREE_USER_MAX_FILE_SIZE = 4194304000
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = get_config("DEF_THUMB_NAIL_VID_S", "https://envs.sh/CQU.jpg")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = get_config("HTTP_PROXY", None)
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # add config vars for the display progress
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▣")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "▢")
+    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
+    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
