709eb0f3b2793357e754f4003db4df84f9f4c650
Krishna <kd259457@gmail.com>
2025-11-26 18:32:17 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,49 @@
+from bot.get_cfg import get_config
+
+class Config(object):
+    #Session
+    SESSION_NAME = get_config("SESSION_NAME", "EncoderX") 
+    #Telegram Credentials 
+    APP_ID = int(get_config("APP_ID", "28891870"))
+    API_HASH = get_config("API_HASH", "ffc3794690bf254d2867ac58fd293a60")
+
+    # Bot Credentials 
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8347616055:AAHxACwlMceprKFi7opb1XTGh3kZzlVcsCE")
+    BOT_USERNAME = get_config("BOT_USERNAME", "Oaid_enc_bot") #Without @
+
+    # User or group id (Seperated by commas 
+    AUTH_USERS = [7660990923, 7465574522]
+
+    #Channels
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "Lod_krishna")
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+
+    #Mongo DB: (Added by @Telegram_Guyz in github 🌚) 
+    MONGO_URI = get_config("MONGO_URI", "mongodb+srv://raj:krishna@cluster0.eq8xrjs.mongodb.net/") #Required 
+    DB_NAME = get_config("DB_NAME", "TGguy") #Required
+    COLLECTION_NAME = get_config("COLLECTION_NAME", "Stores") #Required
+
+    
+    # Download location of your server 
+    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
+    
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 4194304000
+    TG_MAX_FILE_SIZE = 4194304000
+    FREE_USER_MAX_FILE_SIZE = 4194304000
+    UPDATE_INTERVAL = 7
+    
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = get_config("DEF_THUMB_NAIL_VID_S", "https://envs.sh/CQU.jpg")
+    
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = get_config("HTTP_PROXY", None)
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    
+    # add config vars for the display progress
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▣")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "▢")
+    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
+    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
---
+++ bot/localisation.py
@@ -0,0 +1,50 @@
+from bot.get_cfg import get_config
+
+class Localisation:
+    START_TEXT = "Hᴇʟʟᴏ, \n<blockquote>Tʜɪꜱ ɪꜱ ᴀ Tᴇʟᴇɢʀᴀᴍ <b>Video Encoder Bot</b>. \n\n<b>Pʟᴇᴀꜱᴇ Sᴇɴᴅ ᴍᴇ ᴀɴʏ Tᴇʟᴇɢʀᴀᴍ Bɪɢ Vɪᴅᴇᴏ Fɪʟᴇ ɪ ᴡɪʟʟ Cᴏᴍᴘʀᴇꜱꜱ Iᴛ ᴀꜱ  Sᴍᴀʟʟ Vɪᴅᴇᴏ Fɪʟᴇ!</b> \n\n/help Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟꜱ.</blockquote> \n✨ Eɴᴊᴏʏ....."
+   
+    ABS_TEXT = " Please don't be selfish."
+    
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    
+    
+    DOWNLOAD_START = "『 📥  Dᴏᴡɴʟᴏᴀᴅɪɴɢ... ⏳ 』" 
+    
+    UPLOAD_START = "『 📤  Uᴘʟᴏᴀᴅɪɴɢ... ⏳ 』"
+    
+    COMPRESS_START = "『 📀  Tʀʏɪɴɢ ᴛᴏ Eɴᴄᴏᴅᴇ... ⏳ 』"
+    
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    
+    COMPRESS_SUCCESS =  "𝙱𝙾𝚃 𝙱𝚈➛@SECRECT_BOT_UPDATES 𝙼𝚊𝚍𝚎 𝙱𝚢➛ @Lord_Vasudev_Krishna"
+
+    COMPRESS_PROGRESS = "<blockquote>⏳ ETA: {}\n🚀 Pʀᴏɢʀᴇꜱꜱ: {}%</blockquote>"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Cᴜꜱᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Cʟᴇᴀʀᴇᴅ Sᴜᴄᴄᴇꜱꜰᴜʟʟʏ...✅"
+    
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Mᴇᴅɪᴀ Cʟᴇᴀʀᴇᴅ Sᴜᴄᴄᴇꜱꜰᴜʟʟʏ...✅"
+    
+    SAVED_RECVD_DOC_FILE = "Dᴏᴡɴʟᴏᴀᴅᴇᴅ Sᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ...✅"
+    
+    CUSTOM_CAPTION_UL_FILE = " "
+    
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Nᴏ Cᴜꜱᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Fᴏᴜɴᴅ...💔"
+    
+    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+    
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already one Process going on! ⚠️ \n\nCheck Live Status on Encoder Logs ."
+    
+    HELP_MESSAGE = get_config(
+        "STRINGS_HELP_MESSAGE",
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nMaintained By line @SECRECT_BOT_UPDATES"
+    )
+    WRONG_MESSAGE = get_config(
+        "STRINGS_WRONG_MESSAGE",
+        "current CHAT ID: <code>{CHAT_ID}</code>"
+    )
